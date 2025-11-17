400c1347c77f16ed3d6d52173dedcfeaed79d2cc
Amala Sanjay Deshmukh <amalasanjayd@nvidia.com>
2025-11-17 08:37:36 -0800

    Update model name

--- vllm/model_executor/models/registry.py
+++ vllm/model_executor/models/registry.py
@@ -268,7 +268,7 @@ _MULTIMODAL_MODELS = {
     # [Encoder-decoder]
     "DonutForConditionalGeneration": ("donut", "DonutForConditionalGeneration"),
     "NemotronParseForConditionalGeneration": ("nemotron_parse", "NemotronParseForConditionalGeneration"),
-    "NemotronParseLightForConditionalGeneration": ("nemotron_parse_light", "NemotronParseLightForConditionalGeneration"),
+    "NemotronParseLightForConditionalGeneration": ("nemotron_parse_lite", "NemotronParseLightForConditionalGeneration"),
     "Florence2ForConditionalGeneration": ("florence2", "Florence2ForConditionalGeneration"),  # noqa: E501
     "MllamaForConditionalGeneration": ("mllama", "MllamaForConditionalGeneration"),  # noqa: E501
     "Llama4ForConditionalGeneration": ("mllama4", "Llama4ForConditionalGeneration"),  # noqa: E501
